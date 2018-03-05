61275f0efc0c7c25234ecfdd85efe06cde9b49bd
Sean Cooper <sarcoma@live.co.uk>
2018-03-05 15:57:25 +0000

    Update csv_to_html.js

--- tasks/csv_to_html.js
+++ tasks/csv_to_html.js
@@ -2,65 +2,163 @@
  * grunt-csv-to-html
  * https://github.com/bencooling/csv-to-html
  *
+ * Accepts Mustache or Handlebars templates
+ * Register Handlebar helpers
+ * As source files have a template & data file that are distinguished by their extension, can configure accepted extenstions
+ * Configure csv parsing engine
+ * *
+ * For Array to Object see: KooiInc
+ * stackoverflow.com/questions/12199051/merge-two-arrays-of-keys-and-values-to-an-object-using-underscore
+ * jsfiddle.net/KooiInc/DhzqM/
+ *
  * Copyright (c) 2014 Ben Cooling
  * Licensed under the MIT license.
+ *
+ *
+ * This has been hacked together to save one file per row of the csv
+ * Author: Sarcoma
  */
+
+
 
 'use strict';
 
+var mustache   = require('mustache')
+    , Handlebars = require('handlebars')
+    , _          = require('underscore')
+    , csv        = require('csv')
+    , markdown   = require('markdown').markdown
+    , path = require('path');
+
+;
+
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
+    grunt.registerMultiTask('csv_to_html_files', 'Compile HTML files with csv & mustache/handlebars template', function() {
+        var done = this.async()
+            , fi = 1
+            , fl = this.files.length
+            , options = this.options({
+                registerHelpers: false
+                , tpl: false
+                , data: false
+                , dest: false
+                , extensions : {
+                    tpl: ["mustache", "html", "handlebars", "hbs"]
+                    , data: ["csv"]
+                }
+                , csv : {
+                    rowDelimiter : null
+                    , delimiter : ','
+                    , quote : '"'
+                    , escape : '"'
+                    , columns : null
+                    , comment : ''
+                    , objname : false
+                    , trim : false
+                    , ltrim : false
+                    , rtrim : false
+                    , auto_parse : false
+                }
+            })
+            , helpers = options.registerHelpers
+        ;
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
-
-    // Configuration to be run (and then tested).
-    csv_to_html: {
-      example: {
-        options : {
-          registerHelpers:[{ 'lower':function(str){ return str.toLowerCase(); }}
-                         , { 'upper':function(str){ return str.toUpperCase(); }}]
+        // Register custom helpers
+        if (_.isArray(helpers) && helpers.length){
+            _.each(helpers, function(element, index, array){
+                var k = Object.keys(element).toString()
+                    , v = element[k]
+                ;
+                Handlebars.registerHelper(k, v);
+            });
         }
-        , files: {
-          'tmp/compiled.html': ['test/fixtures/tpl.mustache', 'test/fixtures/data-json.csv']
-        }
-      }
-    },
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
+        // Iterate over all specified file groups.
+        this.files.forEach(function(f) {
 
-  });
+            var html = ''
+                , data
+                , tpl
+                , output
+                , src
+            ;
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+            if (options.dest)
+                f.dest = options.dest;
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+            f.src.map(function(filepath) {
+                var extension = _.last(filepath.split("."));
+                if (_.indexOf(options.extensions.data, extension) > -1)
+                    data = filepath;
+                else if (_.indexOf(options.extensions.tpl, extension) > -1)
+                    tpl = filepath;
+            });
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'csv_to_html', 'nodeunit']);
+            if (options.tpl)
+                tpl = options.tpl;
+            if (options.data)
+                data =options.data;
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+            // Validate file accessibility, separate csv from tpl
+            [tpl, data].filter(function(filepath) {
+                grunt.log.writeln(filepath);
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            });
+
+            data = grunt.file.read(data);
+            tpl = grunt.file.read(tpl);
+
+            csv.parse(data, options.csv, function(err, rows){
+
+                var headers = rows.splice(0, 1)[0]
+                    , row
+                ;
+
+                // loop through csv file and compile html
+                for(var i=0,l=rows.length;i<l;i++){
+                    html = ''
+                    row = _.object(headers, rows[i]);
+                    // Any column with json prefix will be parsed as JSON
+                    for (var key in row){
+                        if(/^json_/.test(key)){
+                            row[key.replace('json_','')] = JSON.parse(row[key]);
+                        }
+                        if(/^markdown_/.test(key)){
+                            row[key.replace('markdown_','')] = markdown.toHTML(row[key]);
+                        }
+                    }
+                    output = Handlebars.compile(tpl);
+
+                    html += output(row);
+                    // Write the destination file.
+
+                    var fdir = path.dirname(f.dest);
+                    var fname = path.parse(f.dest).name;
+                    var fext = path.parse(f.dest).ext;
+
+                    grunt.file.write(fdir + '/' + fname + '_' + i + fext, html);
+
+                    // Print a success message.
+                    grunt.log.writeln('File "' + fname + '_' + i + fext + '" created.');
+                }
+
+
+
+                if (fi===fl)
+                    done();
+                else
+                    fi++;
+
+            });
+
+        });
+    });
 
 };
